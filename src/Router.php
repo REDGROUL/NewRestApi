ca2f56b7fd54b153d4805d48931da473b1d8b6b6
redgroul <redgroulman@gmail.com>
2022-04-23 13:35:11 +0300

    add router

---
+++ src/Router.php
@@ -0,0 +1,21 @@
+<?
+
+namespace App;
+
+class Router
+{
+    public function __construct()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+}
